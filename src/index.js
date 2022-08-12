6bbccd060ae0e8080bd3e799152314a0adc78f5e
evgeniya-burlachenko <e.accounted@gmail.com>
2022-08-12 16:18:15 +0300

    hz

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	console.log("str", str)
+	console.log("brack", bracketsConfig)
+	bracketsConfig = bracketsConfig.map(item => item.join(''));
+	console.log("brack2", bracketsConfig)
+
+	while (bracketsConfig.length) {
+	  let iterator = 0;
+  
+	  bracketsConfig.forEach(item => {
+		if (str.indexOf(item) != -1) {
+		  str = str.replace(item, '');
+		  console.log("str2",str)
+		} else {
+		  iterator += 1;
+		}
+	  });
+	  
+	  if (iterator === bracketsConfig.length) {
+		bracketsConfig.length = 0;
+	  }
+	}
+	return str.length === 0 ? true : false;
 }
